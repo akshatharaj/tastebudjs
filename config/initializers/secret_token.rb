e1b4fa3784afaeeb588ef4396fe499781f60204f
Akshatha Raj <akshatharaj31@gmail.com>
2013-05-27 18:19:31 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tastebud::Application.config.secret_token = '07c1b49513bf3f914cc5f8d0b3786cf7bf90a9072fba32c224caf046c84375b5e75cc414b2ae3e0782f217dd6bbd4d0ffd4ad53a8349f7ceda561c6156bc8bb2'

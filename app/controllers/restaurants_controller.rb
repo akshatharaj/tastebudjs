d082c7e68db065f313679937b6de344948ddefe0
Akshatha Raj <akshatharaj31@gmail.com>
2013-05-28 22:24:22 +0000

    initial work on search for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,4 +11,8 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  def search
+    @restaurants = Restaurant.search params[:search]
+   end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :address1, :address2, :city, :name, :other_comments, :phone, :state, :zip
+
+  def self.search(search)
+    search_condition = "%" + search + "%"
+    find(:all, :conditions => ['name LIKE ? OR city LIKE ? or zip LIKE ?', search_condition, search_condition, search_condition])
+  end
+
 end

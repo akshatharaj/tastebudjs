fb6c91bd54fcdb6518789f783ebb81b153706e51
Akshatha Raj <akshatharaj31@gmail.com>
2013-05-29 04:13:22 +0000

    review model fixes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ActiveRecord::Base
   attr_accessible :comment, :rating, :restaurant_id, :user_id
 
-  validate :rating :presence => true
-  validate :restaurant_id :presence => true
-  validate :user_id :presence => true
+  validate :rating, :presence => true
+  validate :restaurant_id, :presence => true
+  validate :user_id, :presence => true
 
 end

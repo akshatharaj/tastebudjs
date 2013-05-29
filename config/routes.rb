a92c9ef58fc165dee0b22650f41edeefc6e4ac50
Akshatha Raj <akshatharaj31@gmail.com>
2013-05-29 05:29:33 +0000

    reviews routes fixes 3

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Tastebud::Application.routes.draw do
   root to: "home#index"
   resources :restaurants
 
-  match '/restaurants/:id/reviews/:id', :to => 'reviews#show', :as => :reviews
+  # match '/restaurants/:id/reviews/:id', :to => 'reviews#show', :as => :reviews
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

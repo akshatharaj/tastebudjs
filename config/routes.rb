6986822ddc80f2a9dafe032cd8da94c57387b3df
Akshatha Raj <akshatharaj31@gmail.com>
2013-06-07 20:52:10 +0000

    fix devise user sign_out issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Tastebud::Application.routes.draw do
     resources :reviews
   end
 
-  devise_for :users
+  devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
 
   root to: "home#index"
 

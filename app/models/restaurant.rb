985f49e45cba24fa424413a741d8eeb089da26ea
Akshatha Raj <akshatharaj31@gmail.com>
2013-06-10 03:38:33 +0000

    Bad move. back up paging changes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,9 +9,9 @@ class Restaurant < ActiveRecord::Base
 
   def self.search(search, page)
     if search
-      where('name ILIKE ? or city ILIKE ? or address1 ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%").paginate(:page => page, :per_page => 5)
+      where('name ILIKE ? or city ILIKE ? or address1 ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%").paginate(:page => page, :per_page => 10)
     else
-      self.paginate(:page => page, :per_page => 5).order('id DESC')
+      self.paginate(:page => page, :per_page => 10).order('id DESC')
     end
   end
 end
